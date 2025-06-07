91c6f6c6cc5aea701e6b6bad0fd933aac148e894
Tony Hunt <tonyjayhunt@googlemail.com>
2025-06-07 09:59:34 +0100

    updated GUI tests

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -1,22 +1,26 @@
 import pytest
-import os
-import tkinter as tk
+from kivy.base import EventLoop
 from gui import JukeboxGUI
 
 @pytest.fixture
-def root():
-    root = tk.Tk()
-    yield root
-    root.destroy()
-
-def test_hidden_song_key_prevents_button(root):
-    # Mocks
+def gui():
+    # Make sure Kivy is initialized for widget creation
+    if not EventLoop.event_listeners:
+        EventLoop.ensure_window()
+    # Create dummy callbacks and player
+    class DummyPlayer: played_songs = []
     def dummy_select(song): pass
     def dummy_special(): pass
-    class DummyPlayer:
-        played_songs = set()
-    gui = JukeboxGUI(root, dummy_select, dummy_special, DummyPlayer())
+    jukebox_gui = JukeboxGUI()
+    jukebox_gui.select_song_cb = dummy_select
+    jukebox_gui.dance_cb = dummy_special
+    jukebox_gui.player = DummyPlayer()
+    return jukebox_gui
+
+def test_hidden_song_key_prevents_button(gui):
+    # Setup
     gui.all_songs = [{'key': 1, 'title': 'T', 'artist': 'A', 'genres': ['pop']}]
-    gui.hidden_song_keys.add(1)
+    gui.hidden_song_keys = [1]
     gui.display_songs()
-    assert 1 not in gui.song_buttons
+    # After display, the grid should have no children since the only song is hidden
+    assert len(gui.songs_grid.children) == 0
